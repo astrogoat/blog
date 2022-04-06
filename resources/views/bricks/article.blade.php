e35e35b6201c22a19626cceca6afcdb879191995
Peter <oniicoder@gmail.com>
2022-04-06 11:59:59 +0000

    Setup article brick

--- resources/views/bricks/article.blade.php
+++ resources/views/bricks/article.blade.php
@@ -3,18 +3,18 @@
     @php($articleInputId = md5($this->_id . '.' . $brickKey))
 
     <div
-        x-on:{{ $larticleInputId }}.window="$wire.set('{{ $brickKey }}', $event.detail)"
+        x-on:{{ $articleInputId }}.window="$wire.set('{{ $brickKey }}', $event.detail)"
     >
         <label for="" class="block text-sm font-medium text-gray-700 pb-1">Article</label>
         @if($this->get($brickKey)->getArticleModel())
             <span class="blog-inline-flex blog-items-center blog-py-0.5 blog-pl-2.5 blog-pr-1 blog-rounded-md blog-text-sm blog-font-medium blog-bg-gray-100 blog-text-gray-700">
-                {{ $this->get($brickKey)->getLocationModel()->name }}
+                {{ $this->get($brickKey)->getArticleModel()->title }}
                 <button
                     type="button"
                     class="blog-flex blog-shrink-0 blog-rounded-md blog-ml-1 blog-h-4 blog-w-4 blog-inline-flex blog-items-center blog-justify-center blog-text-gray-400 hover:blog-bg-gray-200 hover:blog-text-gray-500 focus:blog-outline-none focus:blog-bg-gray-500 focus:blog-text-white"
                     x-on:click="$wire.set('{{ $brickKey }}', null)"
                 >
-                    <span class="blog-sr-only">Remove Location</span>
+                    <span class="blog-sr-only">Remove Article</span>
                     <x-fab::elements.icon icon="x" type="solid" class="blog-h-3 blog-w-3" />
               </button>
             </span>
