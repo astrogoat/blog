33a85848f626ff546117fcf67a9e6ae964843adf
Peter <oniicoder@gmail.com>
2022-04-11 14:23:03 +0000

    Add timestamp to db file names

---
+++ database/migrations/2022_04_06_000000_create_blog_category_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('blog_categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->Text('description')->nullable();
+            $table->string('slug');
+            $table->string('layout')->nullable();
+            $table->unsignedInteger('footer_id')->nullable();
+            $table->timestamps();
+
+            $table->foreign('footer_id')->references('id')->on('footers');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('blog_categories');
+    }
+};
