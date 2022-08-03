c03106d794deefd2794f5d74caa02b21e61e71c2
OniiCoder <oniicoder@gmail.com>
2022-08-03 12:01:01 +0100

    Display stats for groups created

--- resources/views/models/blog/index.blade.php
+++ resources/views/models/blog/index.blade.php
@@ -61,6 +61,45 @@
                     </a>
                 </x-slot>
             </x-fab::layouts.panel>
+
+        </div>
+    </x-fab::layouts.page>
+
+    <x-fab::layouts.page
+        title="Article Groups"
+    >
+        <x-slot name="actions">
+            <x-fab::elements.button type="link" :url="route('lego.blog.groups.create')">Create</x-fab::elements.button>
+        </x-slot>
+        <div class="mt-2 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+
+            @foreach($groups as $group)
+                <x-fab::layouts.panel>
+                    <div class="flex items-center">
+                        <div class="flex-shrink-0">
+                            <x-fab::elements.icon icon="document-text" class="h-6 w-6 text-gray-400"/>
+                        </div>
+                        <div class="ml-5 w-0 flex-1">
+                            <dl>
+                                <dt class="text-sm font-medium text-gray-500 truncate">
+                                    {{ $group->title }}
+                                </dt>
+                                <dd>
+                                    <div class="text-lg font-medium text-gray-900">
+                                        {{ $group->articles->count() }}
+                                    </div>
+                                </dd>
+                            </dl>
+                        </div>
+                    </div>
+                    <x-slot name="footer">
+                        <a href="{{ route('lego.blog.groups.edit', $group) }}"
+                           class="flex items-center justify-end text-sm text-gray-600">Edit
+                            <x-fab::elements.icon icon="arrow-narrow-right" class="ml-2 h-5 w-5 text-gray-400"/>
+                        </a>
+                    </x-slot>
+                </x-fab::layouts.panel>
+            @endforeach
         </div>
     </x-fab::layouts.page>
 @endsection
