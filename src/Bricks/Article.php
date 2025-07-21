4cd96b77912c32849b3b73598b9c6486625078c2
Laura Tonning <github@tonning.dev>
2025-07-21 14:43:23 +0200

    Update Article brick

--- src/Bricks/Article.php
+++ src/Bricks/Article.php
@@ -6,11 +6,6 @@ use Helix\Lego\Bricks\Brick;
 
 class Article extends Brick
 {
-    public function getDefaults()
-    {
-        return $this->default;
-    }
-
     public function brickView(): string
     {
         return 'blog::bricks.article';
